cbe016a19b4bd8af7f0108f897c82760b2a6d7d5
Jean Haro <JeanCHL98@hotmail.com>
2022-02-10 22:33:45 -0500

    Implementando las rutas principales

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+/* CommonModule - nos permite realizar *ngIf y *ngFor */ 
+// import { CommonModule } from '@angular/common';
+
+/* RouterModule - es lo que necesitamos exportar para que otro modulo lo pueda disponer de el */
+import { RouterModule, Routes } from '@angular/router';
+
+// Componentes
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
+// Rutas de la aplicación
+const routes: Routes = [
+  /* path inicial */
+  { path: 'dashboard', component: DashboardComponent },
+
+  { path: 'login', component: LoginComponent },
+
+  { path: 'register', component: RegisterComponent },
+
+  { path: 'progress', component: ProgressComponent },
+
+  { path: 'grafica1', component: Grafica1Component },
+
+  /* Cuando estoy en la ruta con el slash vacío va a redireccionar automáticamente al dashboard */
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  /* Si es que la ruta que se coloca en el slash, no es de ninguno de acá se va a redirigir al componente de Nopagefound */
+  { path: '**', component: NopagefoundComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    /* CommonModule */
+    // forRoot - es para rutas principales 
+    RouterModule.forRoot( routes )
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Routing */
+import { AppRoutingModule } from './app-routing.module';
+
+/* Componentes */
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -9,6 +13,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
 
 @NgModule({
   declarations: [
@@ -19,10 +25,13 @@ import { HeaderComponent } from './shared/header/header.component';
     DashboardComponent,
     BreadcrumbsComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
