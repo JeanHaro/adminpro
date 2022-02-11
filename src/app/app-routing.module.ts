3b949840e2ae9004be5d5e644c4ff0922087490e
Jean Haro <JeanCHL98@hotmail.com>
2022-02-10 23:42:22 -0500

    Implementar rutas secundarias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,22 +12,29 @@ import { RegisterComponent } from './auth/register/register.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 // Rutas de la aplicación
 const routes: Routes = [
+  /* Rutas protegidas */
   /* path inicial */
-  { path: 'dashboard', component: DashboardComponent },
+  { 
+    path: '', 
+    component: PagesComponent,
+    /* Rutas hijas */ 
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      /* Cuando estoy en la ruta con el slash vacío va a redireccionar automáticamente al dashboard */
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
 
+  /* Rutas publicas */
   { path: 'login', component: LoginComponent },
-
   { path: 'register', component: RegisterComponent },
 
-  { path: 'progress', component: ProgressComponent },
-
-  { path: 'grafica1', component: Grafica1Component },
-
-  /* Cuando estoy en la ruta con el slash vacío va a redireccionar automáticamente al dashboard */
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   /* Si es que la ruta que se coloca en el slash, no es de ninguno de acá se va a redirigir al componente de Nopagefound */
   { path: '**', component: NopagefoundComponent }
 ]
