23b0d6740553e24f585e043b3aaea7f298b5415f
Jean Haro <JeanCHL98@hotmail.com>
2022-02-18 17:34:56 -0500

    Rutas hijas - ForChild()

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,32 +5,16 @@ import { NgModule } from '@angular/core';
 /* RouterModule - es lo que necesitamos exportar para que otro modulo lo pueda disponer de el */
 import { RouterModule, Routes } from '@angular/router';
 
+// Routing Modules
+import { PagesRoutingModule } from './pages/pages.routing';
+
 // Componentes
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 // Rutas de la aplicación
 const routes: Routes = [
-  /* Rutas protegidas */
-  /* path inicial */
-  { 
-    path: '', 
-    component: PagesComponent,
-    /* Rutas hijas */ 
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      /* Cuando estoy en la ruta con el slash vacío va a redireccionar automáticamente al dashboard */
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-
   /* Rutas publicas */
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -43,7 +27,8 @@ const routes: Routes = [
   imports: [
     /* CommonModule */
     // forRoot - es para rutas principales 
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule
   ],
   exports: [ RouterModule ]
 })
