99f841b46bc56e2ed6e2fca4278b50871b9b861f
Jean Haro <JeanCHL98@hotmail.com>
2022-03-04 01:26:55 -0500

    Cambiar el CSS principal de forma dinámica

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
+  // Para obtener el selextor link del CSS
+  public linkTheme = document.querySelector('#theme');
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeTheme ( theme: string ) {
+    const url = `assets/css/colors/${theme}.css`;
+
+    // Enviamos el atributo cambiado
+    this.linkTheme?.setAttribute('href', url);
+
+    // Guardar en el LocalStorage
+    localStorage.setItem('theme', url);
+  }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
+  // Para obtener el selextor link del CSS
+  public linkTheme = document.querySelector('#theme');
+
   constructor() { }
 
   ngOnInit(): void {
+    const url = localStorage.getItem('theme') || 'assets/css/colors/purple-dark.css' ; 
+
+    // Enviamos el atributo cambiado
+    this.linkTheme?.setAttribute('href', url);
   }
 
 }
