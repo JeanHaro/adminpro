bec4279cf2ba87cf25f75cd4bf6ad843ecf96847
Jean Haro <JeanCHL98@hotmail.com>
2022-03-15 13:24:57 -0500

    Componente Rxjs y arreglo en el menu

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,13 +9,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PromesasComponent } from './promesas/promesas.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PagesComponent } from './pages.component';
-
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 // Modulos
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   // Solo va a funcionar interna en el mismo modulo
@@ -25,7 +25,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
-    PromesasComponent
+    PromesasComponent,
+    RxjsComponent
   ],
   // Exportamos los componentes para poder usarlos en otros lados
   exports: [
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -10,6 +10,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const routes: Routes = [
     /* Rutas protegidas */
@@ -25,7 +26,8 @@ const routes: Routes = [
             { path: 'progress', component: ProgressComponent },
             { path: 'grafica1', component: Grafica1Component },
             { path: 'account-settings', component: AccountSettingsComponent },
-            { path: 'promesas', component: PromesasComponent }
+            { path: 'promesas', component: PromesasComponent },
+            { path: 'rxjs', component: RxjsComponent }
             /* Cuando estoy en la ruta con el slash vacío va a redireccionar automáticamente al dashboard */
             // { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Esto se colocará en el Routing general (app-routing) por el path vacio
         ]
