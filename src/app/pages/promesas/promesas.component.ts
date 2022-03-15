e01e1b32dc8d697d31378aaef54794ae9adf0005
Jean Haro <JeanCHL98@hotmail.com>
2022-03-15 12:41:19 -0500

    Funciones que retornan promesas

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -11,49 +11,24 @@ export class PromesasComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const promesa = new Promise((resolve, reject) => {
-      if (false) {
-        resolve('Hola Mundo');
-      } else {
-        reject('Algo salió mal');
-      }
-    });
-
-    promesa.then((mensaje) => {
-      console.log(mensaje);
+    // this.getUsuarios();  
+    this.getUsuarios().then(usuarios => {
+      console.log(usuarios);
     })
-    .catch(error => console.log('Error en mi promesa:', error));
+  }
 
-    console.log('Fin del Init');
+  getUsuarios() {
+    const promesa = new Promise(resolve => {
+      fetch('https://reqres.in/api/users')
+      // then() - información propia de cuando se resuelva esta peticion
+      // El json regresa otra promesa
+      .then(resp => resp.json())
+      // resolve manda lo que va a mostrar
+      .then(body => resolve(body.data))
+    })
 
-    /*
-    // Si no se coloca el resolve en la Promise, seguirá ejecutando de manera sincrona
-    const promesa = new Promise(() => {
-        console.log('Hola Mundo');
-    });
-    console.log('Fin del Init');
-    --> Hola Mundo
-    --> Fin del Init
-
-    // Si la promesa está correcta, nos ejecuta el resolve
-    if -> true
-    Fin del Init
-    Hola mundo 
-
-    // Sin catch nos aparece con rojo los errores
-    if -> false
-    Fin del Init
-    core.mjs:6485 ERROR Error: Uncaught (in promise): Algo salió mal
-    .....
-
-    // Con catch, nos aparece el error solamente con el mensaje que se colocó
-    if -> false
-    Fin del Init
-    Error en mi promesa: Algo salió mal
-    */
-
-    // La aplicación seguirá funcionando igualmente con el error que presente
-  
+    // retorna el body.data
+    return promesa;
   }
 
 }
