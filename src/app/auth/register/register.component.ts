98bb0a8da76cf97dd7b58cadb6980a2fa585bd69
Jean Haro <JeanCHL98@hotmail.com>
2022-10-25 04:42:04 -0500

    Validaciones del formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -10,19 +10,46 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
   // TODO: Como quiero que luzca mi formulario
   public registerForm = this.fb.group({
     // En la segunda parte del array se coloca el Validators para que valide el valor
-    nombre: ['Jean', [Validators.required]],
-    email: ['jeanch447@gmail.com', Validators.required],
-    password: ['123456', [Validators.required]],
-    password2: ['123456', Validators.required],
+    nombre: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    password2: ['', Validators.required],
     terminos: [false, Validators.required]
   });
 
   constructor (private fb: FormBuilder) { }
 
   crearUsuario() {
+    // TODO: Una manera rápida de saber de cuando el formulario se envió
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    // Verificar que el fromulario sea correcto al momento de enviar
+    if (this.registerForm.valid) {
+      console.log('Posteando formulario');
+    } else {
+      console.log('Formulario no es correcto...');
+    }
+  }
+
+  // TODO: Si el campo  no es valido
+  campoNoValido (campo: string): boolean {
+    // Si el campo se envió y el campo no es valido
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // TODO: Si los términos es valido
+  aceptaTerminos (): boolean {
+    // Si los términos es false y el campo se envió
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 }
