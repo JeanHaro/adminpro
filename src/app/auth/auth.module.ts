99cf7b549f6bcdcf46fc945814aab6b8b9fc5bd5
Jean Haro <JeanCHL98@hotmail.com>
2022-10-25 03:33:38 -0500

    Formulario de registro - Reactive Forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 // Componentes
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
+// Modulos 
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -18,7 +21,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// Formularios
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  // TODO: Como quiero que luzca mi formulario
+  public registerForm = this.fb.group({
+    // En la segunda parte del array se coloca el Validators para que valide el valor
+    nombre: ['Jean', [Validators.required]],
+    email: ['jeanch447@gmail.com', Validators.required],
+    password: ['123456', [Validators.required]],
+    password2: ['123456', Validators.required],
+    terminos: [false, Validators.required]
+  });
 
-  ngOnInit(): void {
+  constructor (private fb: FormBuilder) { }
+
+  crearUsuario() {
+    console.log(this.registerForm.value);
   }
-
 }
