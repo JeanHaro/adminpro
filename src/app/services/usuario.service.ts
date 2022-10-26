f6cf0da6400ec500ea895eda87bcf558b83f7706
Jean Haro <JeanCHL98@hotmail.com>
2022-10-26 03:06:48 -0500

    Guardar informacion en el LocalStorage

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+// Rxjs
+import { tap } from 'rxjs/operators';
+
 // Interfaces
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
@@ -24,11 +27,25 @@ export class UsuarioService {
     // Como es una petición post tenemos que mandar la data, y eso se envía en el segúndo argumento
     // Como es una promesa el post(), tendríamos que subscrirnos a esto
     // Pero colocando el return haríamos el subscribe en el otro archivo donde haremos la petición 
-    return this.http.post(`${base_url}/usuarios`, formData);
+    return this.http.post(`${base_url}/usuarios`, formData).pipe(
+      // tap() - va a recibir lo que responda la petición
+      tap((resp: any) => {
+        // Primer parámetro - es la llave donde lo vamos a guardar
+        // Segundo parámetro - es lo que quiero grabar y solo puede almacenar string
+        localStorage.setItem('token', resp.token)
+      })
+    )
   }
 
   // TODO: Iniciar sesión
   login (formData: LoginForm) {
-    return this.http.post(`${base_url}/login`, formData);
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      // tap() - va a recibir lo que responda la petición
+      tap((resp: any) => {
+        // Primer parámetro - es la llave donde lo vamos a guardar
+        // Segundo parámetro - es lo que quiero grabar y solo puede almacenar string
+        localStorage.setItem('token', resp.token)
+      })
+    )
   }
 }
