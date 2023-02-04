cda73604e1f62b1281ceba6f6d899c1b5f23a8e2
Jean Haro <JeanCHL98@hotmail.com>
2023-02-04 02:55:26 -0500

    CRUD de hospitales

--- src/app/services/hospital.service.ts
+++ src/app/services/hospital.service.ts
@@ -35,12 +35,33 @@ export class HospitalService {
     }
   }
 
-  // TODO: Cargar usuarios
+  // TODO: Cargar hospitales
   cargarHospitales() {
     const url = `${base_url}/hospitales`;
 
     return this.http.get<{ ok: boolean, hospitales: Hospital[] }>(url, this.headers).pipe(
       map((resp: { ok: boolean, hospitales: Hospital[] }) => resp.hospitales)
     );
+  }
+
+  // TODO: Crear hospital
+  crearHospital (nombre: string) {
+    const url = `${base_url}/hospitales`;
+
+    return this.http.post(url, { nombre }, this.headers);
+  }
+
+  // TODO: Actualizar hospital
+  actualizarHospital (_id: string, nombre: string) {
+    const url = `${base_url}/hospitales/${_id}`;
+
+    return this.http.put(url, { nombre }, this.headers);
+  }
+
+  // TODO: Eliminar hospital
+  eliminarHospital (_id: string) {
+    const url = `${base_url}/hospitales/${_id}`;
+
+    return this.http.delete(url, this.headers);
   }
 }
