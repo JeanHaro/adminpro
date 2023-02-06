221ef65eb33dd41fff5d6a99a64ce0a9930861fa
Jean Haro <JeanCHL98@hotmail.com>
2023-02-06 03:53:37 -0500

    Servicio de Médicos - CRUD

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+// Modelo
+import { Medico } from 'src/app/models/medico.model';
+
+// Servicios
+import { MedicoService } from 'src/app/services/medico.service';
+import { ModalImagenService } from 'src/app/services/modal-imagen.service';
+
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
@@ -8,9 +15,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicosComponent implements OnInit {
 
-  constructor() { }
+  public medicos: Medico[] = [];
+  public medicosTemp: Medico[] = [];
+  public cargando: boolean = true;
+
+  constructor (
+    private medicoService: MedicoService,
+    private modalImagenService: ModalImagenService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarHospitales();
+  }
+
+  // TODO: Cargar hospitales
+  cargarHospitales() {
+    this.cargando = true;
+
+    this.medicoService.cargarNMedicos().subscribe({
+      next: (medicos) => {
+        this.cargando = false;
+        this.medicos = medicos;
+        this.medicosTemp = medicos;
+      }
+    })
+  }
+
+  // TODO: Actualizar foto
+  abrirModal (medico: Medico) {
+    this.modalImagenService.abrirModal('medicos', medico._id, medico.img);
   }
 
 }
--- src/app/services/medico.service.ts
+++ src/app/services/medico.service.ts
@@ -1,9 +1,66 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+// Rxjs
+import { map } from 'rxjs';
+
+// Environment
+import { environment } from 'src/environments/environment';
+
+// Modelo
+import { Medico } from '../models/medico.model';
+
+const base_url = environment.base_url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
 
-  constructor() { }
+  constructor (
+    private http: HttpClient
+  ) { }
+
+  // TODO: Obtener el token del localStorage
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token
+      },
+    }
+  }
+
+  // TODO: Cargar medicos
+  cargarNMedicos() {
+    const url = `${base_url}/medicos`;
+
+    return this.http.get<{ ok: boolean, medicos: Medico[] }>(url, this.headers).pipe(
+      map((resp: { ok: boolean, medicos: Medico[] }) => resp.medicos)
+    );
+  }
+
+  // TODO: Crear hospital
+  crearMedico (medico: Medico) {
+    const url = `${base_url}/medicos`;
+
+    return this.http.post(url, medico, this.headers);
+  }
+
+  // TODO: Actualizar hospital
+  actualizarMedico (medico: Medico) {
+    const url = `${base_url}/medicos/${medico._id}`;
+
+    return this.http.put(url, medico, this.headers);
+  }
+
+  // TODO: Eliminar hospital
+  eliminarMedico (_id: string | undefined) {
+    const url = `${base_url}/medicos/${_id}`;
+
+    return this.http.delete(url, this.headers);
+  }
 }
